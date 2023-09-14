5baf7685e14fcaa20cdd229b772f9a79a8184db5
thuhang2510 <tunguyet2510@gmail.com>
2023-09-14 23:22:30 +0700

    feat(BFS): add method travesal graph according to bfs

---
+++ Graphix/BFS.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphix
+{
+    internal class BFS
+    {
+        private Graph g;
+
+        public BFS(Graph g)
+        {
+            this.g = g;
+        }
+
+        public void StartBFS(int startVertex)
+        {
+            bool[] visited = new bool[g.n];
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(startVertex);
+            visited[startVertex] = true;
+
+            while (queue.Count > 0)
+            {
+                int u = queue.Dequeue();
+                Console.WriteLine(g.vertices[u]);
+
+                for (int v = 0; v < g.n; ++v)
+                    if (g[u, v] > 0 && visited[v] == false)
+                    {
+                        visited[v] = true;
+                        queue.Enqueue(v);
+                    }
+            }
+        }
+    }
+}
