61789379554253ae0b2bbfba0303eb85d8673da9
thuhang2510 <tunguyet2510@gmail.com>
2023-09-14 23:20:15 +0700

    feat(FileIO): add method relate to File and add try/catch

---
+++ Graphix/FileIO.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphix
+{
+    internal class FileIO
+    {
+        public static (string, int) WriteFile(string fileName, string[] lines)
+        {
+            try
+            {
+                File.WriteAllLines(fileName, lines);
+            }
+            catch (IOException e)
+            {
+                return (e.Message, -1);
+            }
+
+            return ("write data to file success", 0);
+        }
+
+        public static (string, int) DeleteFile(string fileName)
+        {
+            try
+            {
+                File.Delete(fileName);
+            }
+            catch (IOException e)
+            {
+                return (e.Message, -1);
+            }
+
+            return ("delete file success", 0);
+        }
+
+        public static (string[], int) ReadFile(string fileName)
+        {
+            string[] lines;
+
+            try
+            {
+                lines = File.ReadAllLines(fileName);
+            }
+            catch (IOException)
+            {
+                return (null, -1);
+            }
+
+            return (lines, 0);
+        }
+    }
+}
