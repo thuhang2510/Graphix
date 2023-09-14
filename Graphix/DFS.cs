055885725ed033705920cde936aafb6c3221c370
thuhang2510 <tunguyet2510@gmail.com>
2023-09-14 23:22:02 +0700

    feat(DFS): add method travesal graph according to dfs

---
+++ Graphix/DFS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphix
+{
+    internal class DFS
+    {
+        private Graph g;
+        private bool[] visited;
+
+        public DFS(Graph g)
+        {
+            visited = new bool[g.n];
+            this.g = g;
+        }
+
+        public void StartDFS(int u)
+        {
+            visited = new bool[g.n];
+            Dfs(u);
+        }
+
+        private void Dfs(int u)
+        {
+            visited[u] = true;
+            Console.WriteLine(g.vertices[u]);
+
+            for (int v = 0; v < g.n; ++v)
+                if (g[u, v] > 0 && visited[v] == false)
+                    Dfs(v);
+        }
+    }
+}
